882d915223a1cef6663944db72fa766e6af2c7be
Clhyzer <fatur.trukan@gmail.com>
2026-04-27 09:14:12 +0700

    navbar

--- java.js
+++ java.js
@@ -1,8 +1,34 @@
-let slides = document.querySelectorAll('.slide');
+let slides = document.querySelectorAll(".slide");
 let index = 0;
 
 setInterval(() => {
-  slides[index].classList.remove('active');
+  slides[index].classList.remove("active");
   index = (index + 1) % slides.length;
-  slides[index].classList.add('active');
-}, 3000); // ganti tiap 3 detik
+  slides[index].classList.add("active");
+}, 3000);
+
+const sections = document.querySelectorAll("#hero, #menu, #lokasi, #sosial");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 100;
+    const sectionHeight = section.offsetHeight;
+
+    if (
+      window.scrollY >= sectionTop &&
+      window.scrollY < sectionTop + sectionHeight
+    ) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
